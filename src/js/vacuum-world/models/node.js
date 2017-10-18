31eb08638c0ccad33e7aebf28b3d52b3378dc6f2
Simon AM <purrcat259@users.noreply.github.com>
2017-10-18 15:53:51 +0200

    Added node representation printing

--- src/js/vacuum-world/models/node.js
+++ src/js/vacuum-world/models/node.js
@@ -3,4 +3,15 @@ export default class Node {
         this.state = state;
         this.parentNode = parentNode;
     }
+
+    print() {
+        for (let i = 0; i < this.state.length; i++) {
+            let rowString = '';
+            for (let j = 0; j < this.state[0].length; j++) {
+                rowString = `${rowString} | ${this.state[i][j]}`;
+            }
+            rowString = `${rowString} |`;
+            console.log(rowString);
+        }
+    }
 }
--- src/js/vacuum-world/vacuum-world.js
+++ src/js/vacuum-world/vacuum-world.js
@@ -1,12 +1,54 @@
 import Node from './models/node';
 
+/*
+* State is a 2D array of integers which is represented using the following numbers:
+* Clean = 0
+* Dirty = 1
+* Clean with Vacuum in the same tile = 2
+* Dirty with Vacuum in the same tile = 3
+*/
+
+/*
+* Actions are represented as strings:
+* ML: Move Left
+* MR: Move right
+* MU: Move up
+* MD: Move Down
+* C: Clean
+*/
+
 export default class VacuumWorld {
     constructor(rows, columns) {
         this.rows = rows;
         this.columns = columns;
+        this.currentState = [];
+        this.initialNode = null;
+        this.currentNode = null;
+    }
+
+    generate() {
+        console.log(`Generating world with ${this.rows} rows and ${this.columns} columns`);
+        // for (let i = 0; i < this.rows; i++) {
+        //     // Generate an empty array to represent the row
+        //     this.currentState.push([]);
+        //     for (let j = 0; j < this.columns; j++) {
+        //         // Have a 33% chance of the tile generated being dirty
+        //         this.currentState[i].push();
+        //     }
+        // }
+        // Hardcoded initial state:
+        // TODO: generate it randomly
+        this.currentState = [
+            [1, 1],
+            [1, 3]
+        ];
+        this.initialNode = new Node(JSON.parse(JSON.stringify(this.currentState)), null);
+        this.currentNode = this.initialNode;
+        console.log('Initial state: ');
+        this.currentNode.print();
     }
 
     run() {
-        console.log(`Starting from world with ${this.rows} rows and ${this.columns} columns`);
+        this.generate();
     }
 }
