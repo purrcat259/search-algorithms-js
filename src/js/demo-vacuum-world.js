42de7b555a5a0d3b467be86a5926b1f2ea4e8251
Simon AM <purrcat259@users.noreply.github.com>
2017-11-01 16:33:37 +0100

    Run after pressing button

--- src/js/demo-vacuum-world.js
+++ src/js/demo-vacuum-world.js
@@ -1,5 +1,6 @@
 import VacuumWorld from './vacuum-world/vacuum-world';
 
-let world = new VacuumWorld(3, 3);
-
-world.run();
+document.getElementById('startButton').addEventListener('click', () => {
+    let world = new VacuumWorld(3, 3);
+    world.run();
+});
