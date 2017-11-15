3de56b4797cdb9d0967d1c7603b8a0d624351ae9
Simon AM <purrcat259@users.noreply.github.com>
2017-11-15 14:47:18 +0100

    Added fusebox config to minify output

---
+++ fuse.prod.js
@@ -0,0 +1,36 @@
+const {
+    FuseBox,
+    HTMLPlugin,
+    WebIndexPlugin,
+    BabelPlugin,
+    SassPlugin,
+    CSSPlugin,
+    QuantumPlugin
+} = require("fuse-box");
+
+const fuse = FuseBox.init({
+    homeDir: 'src',
+    output: 'dist/$name.js',
+    useTypescriptCompiler: true,
+    sourceMaps: true,
+    target: 'browser',
+    plugins: [
+        WebIndexPlugin({
+            template: './src/vacuum-world.html'
+        }),
+        [SassPlugin(), CSSPlugin()],
+        BabelPlugin(),
+        QuantumPlugin({
+            target: 'browser',
+            bakeApiIntoBundle : 'client/app',
+            containedAPI : true,
+            treeshake : true,
+            uglify : true
+        })
+    ]
+});
+
+fuse.bundle('client/app')
+    .instructions('> js/demo-vacuum-world.js');
+
+fuse.run();
